3996639fdce7ae860ba1808c01a2ca5a4bdacf28
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-27 20:25:41 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,7 +4,7 @@ def my_hash
   house_rooms = { "bedrooms" => 4, "utility" => 1, "kitchen" => 1, "sunroom" => 1, "garage" => 0 }
 end
 
-  # Set a variable called `the_manifest` to a hash with 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+# Set a variable called `the_manifest` to a hash with 5 whale bone corsets, 2 porcelain vases, and 3 oil paintings 
 def shipping_manifest
   the_manifest = {
     "whale bone corsets" => 5, 
@@ -13,15 +13,14 @@ def shipping_manifest
   }
 end
 
+# Look up the value of the "oil paintings" key in the shipping_manifest
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifest = { 
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2, 
+    "oil paintings" => 3
+  }
+  shipping_manifest["oil paintings"]
 end
 
 def adding
